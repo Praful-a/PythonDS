2411b8f6407ade1038a6234b2d1016f51d209f32
rock <prafuldubey27@gmail.com>
2021-06-11 22:56:28 +0530

    selection Sort

--- sorting/selection.py
+++ sorting/selection.py
@@ -3,15 +3,15 @@ def printArr(arr, n):
     for i in range(n):
         print(arr[i], end=" ")
 
-def insertionSort(arr, n):
+def selectionSort(arr, n):
+    
+    for i in range(n - 1):
+        min_ind = i
+        for j in range(i + 1, n):
+            if(arr[j] < arr[min_ind]):
+                min_ind = j
+        arr[min_ind], arr[i] = arr[i], arr[min_ind]
 
-    for i in range(1, n):
-        temp = arr[i]
-        j = i - 1
-        while(j >= 0 and arr[j] > temp):
-            arr[j+1] = arr[j]
-            j = j - 1
-        arr[j+1] = temp
 
 if __name__ == '__main__':
     arr = []
@@ -21,6 +21,6 @@ if __name__ == '__main__':
         arr.append(int(input()))
     print(" .. Before Sorting ..")
     printArr(arr, size)
-    insertionSort(arr, size)
+    selectionSort(arr, size)
     print("\n\n.. After Sorting ..")
     printArr(arr, size)
