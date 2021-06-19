9fb4d3b963c32ca87654cf95bec3909a66a09335
rock <prafuldubey27@gmail.com>
2021-06-19 18:25:56 +0530

    kthSmallest problem

--- arrays/kthSmall.py
+++ arrays/kthSmall.py
@@ -1,12 +1,16 @@
 # Method first 
+class solution: 
+    def kthsmallest(self, arr, l, r, k):
+        for i in range(l, r):
+            for j in range(0, r-i):
+                if(arr[j] > arr[j+1]):
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        return arr[k-1]
 
-def kthSmall(arr, k):
-    arr.sort()
-    return arr[k-1]
 
 if __name__ == '__main__':
-    arr = [7, 10, 4, 3, 20, 15]
-    print(" Enter the no. how small  value you want?")
+    t = int(input())
+    arr = list(map(int, input().strip().split()))
     k = int(input())
-    res = kthSmall(arr, k)
-    print("3rd smallest element in the given array is: ", res)
+    ob = solution()
+    print(ob.kthsmallest(arr, 0, t-1, k))
---
+++ stack/stack.py
@@ -0,0 +1,5 @@
+import array as arr
+
+a = arr.array('d', [])
+a[0] = 1
+print(a)
