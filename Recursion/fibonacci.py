21fe4b68597c291c04458d325e8f8cd2e9c72d3d
Praful-a <prafuldubey27@gmail.com>
2021-08-07 22:52:18 +0530

    solve fibonacci with memoization

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -1,3 +1,4 @@
+"""
 def fib(n):
     a, b = 0, 1
     for _ in range(2, n + 1):
@@ -5,8 +6,29 @@ def fib(n):
         a = b
         b = c
     return c
+"""
+
+"""
+def fib(n):
+    if (n <= 1):
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+"""
+
+# Recursion using memoization
 
 
+def fib(n):
+    if (n <= 1):
+        return n
+    if (memo[n] != -1):
+        return memo[n]
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+
+
+memo = [-1] * 200
 num = int(input("Enter a number : "))
 res = fib(num)
 print(f"The fibonacci of {num} is : {res}")
