188195e072424f7e213ac9dc2a3c16539a328beb
Praful-a <prafuldubey27@gmail.com>
2022-01-16 17:08:04 +0530

    pattern2 created

--- Problems/patterns/numPatrn.py
+++ Problems/patterns/numPatrn.py
@@ -5,10 +5,25 @@
 12
 1
 '''
-def pattern1(n):
-	for row in range(n, 0, -1):
-		for col in range(1, row+1):
+# def pattern1(n):
+# 	for row in range(n, 0, -1):
+# 		for col in range(1, row+1):
+# 			print(col, end="")
+# 		print()
+
+# pattern1(5)
+
+'''
+12345
+2345
+345
+45
+5
+'''
+def pattern2(n):
+	for row in range(1, n+1):
+		for col in range(row, n+1):
 			print(col, end="")
 		print()
 
-pattern1(5)
+pattern2(5)
