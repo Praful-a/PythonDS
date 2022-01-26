66ba92c82a595a054a5dae4e2e591a1a52348da3
Praful-a <prafuldubey27@gmail.com>
2022-01-26 15:33:43 +0530

    maximum-element-in-array-using-recursion

---
+++ recursion/maxOfArr.py
@@ -0,0 +1,13 @@
+def maxOfArr(arr, idx):
+	if idx == len(arr) - 1:
+		return arr[idx]
+
+	misa = maxOfArr(arr, idx + 1)
+	if (misa > arr[idx]):
+		return misa 
+	else:
+		return arr[idx]
+
+
+arr = [220, 33, 40, 19, 7]
+print(maxOfArr(arr, 0))
